e54d13bcdd168a7c8cc31e1cd0c93750b3d065f6
srikanthnagudu2909 <nagudusrikanth@gmail.com>
2025-09-10 13:06:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 function getFormvalue() {
     //Write your code here
-	 e.preventDefault()
 	const form=document.getElementById("form1")
-    //let fname=e.target[0].
 	fname=form.elements['fname'].value
-	fname=fname.trim()
-	//let lname=e.target[1].value
 	lname=form.elements['lname'].value
+	fname=fname.trim()
 	lname=lname.trim()
 	alert(`${fname} ${lname}`)
 }
- // const container=document.getElementById("form1")
- // container.addEventListener("submit",getFormvalue)
+ 
